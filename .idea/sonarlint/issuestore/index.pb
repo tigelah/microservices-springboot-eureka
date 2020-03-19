afb81d5ac78929b37c97fa6005135e56631f3ff3
tigelah <lokoportitimao2009@hotmail.com>
2020-03-19 15:48:21 -0300

    finally circuitbreaker

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+ç
+]discovery-server/src/main/java/br/com/rodrigo/discoveryserver/DiscoveryServerApplication.java,2/2/220fc0aa828b6a9f0a0fcfbfc8f8c43146f47864
--- movie-catalog-service/src/main/java/br/com/rodrigo/moviecatalogservice/services/MovieInfo.java
+++ movie-catalog-service/src/main/java/br/com/rodrigo/moviecatalogservice/services/MovieInfo.java
@@ -1,5 +1,6 @@
 package br.com.rodrigo.moviecatalogservice.services;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,7 +17,14 @@ public class MovieInfo {
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
+	@HystrixCommand(fallbackMethod = "getFallbackCatalogItem",
+			threadPoolKey = "movieInfoPool",
+			threadPoolProperties = {
+			@HystrixProperty(name = "coreSize", value = "20"),
+			@HystrixProperty(name = "maxQueueSize" , value = "10")
+
+			}
+	)
 	public CatalogItem getCatalogItem(Rating rating) {
 		Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
 		return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
